c80d4d9ae5163b15a030f52a96b2392c6a663304
vladislav-chihun <vladikkosmo@gmail.com>
2024-07-20 17:39:55 +0200

    create step 2

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 import express from "express";
+import pino from "pino-http";
+import cors from "cors"
+
 const PORT = process.env.PORT || 3000;
 function setupServer() {
     const app = express();
 
+    app.use(cors());
+
+    app.use(pino());
+
     app.get("/", (req, res) => {
         res.send("Success");
+    });
+
+    app.use("*",(req, res, next) => {
+        res.status(404).send({ message: 'Not found', }
+        );
+    });
+
+    app.use((error, req, res, next) => {
+        res.status(500).send("Iternal Server Error");
     });
 
     app.listen(PORT, () => {
