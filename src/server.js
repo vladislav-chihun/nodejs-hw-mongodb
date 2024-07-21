a85ddf893993c069f08be02f1023ea79a8821427
vladislav-chihun <vladikkosmo@gmail.com>
2024-07-21 19:13:05 +0200

    create step 5

--- src/server.js
+++ src/server.js
@@ -1,37 +1,47 @@
-import express from "express";
-import pino from "pino-http";
-import cors from "cors";
-import "dotenv/config";
-
-
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import 'dotenv/config';
+import { Contact } from './models/contact.js';
 
 const PORT = process.env.PORT || 3000;
 
+function setupServer() {
+  const app = express();
 
+  app.use(cors());
 
- function setupServer() {
-    const app = express();
+  app.use(pino());
 
-    app.use(cors());
+  app.get('/', (req, res) => {
+    res.send('Success');
+  });
 
-    app.use(pino());
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await Contact.find();
+      res.send({
+        status: res.statusCode,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status('500').send('Iteranl Server Error');
+    }
+  });
 
-    app.get("/", (req, res) => {
-        res.send("Success");
-    });
+  app.use('*', (req, res, next) => {
+    res.status(404).send({ message: 'Not found' });
+  });
 
-    app.use("*",(req, res, next) => {
-        res.status(404).send({ message: 'Not found', }
-        );
-    });
+  app.use((error, req, res, next) => {
+    res.status(500).send('Iternal Server Error');
+  });
 
-    app.use((error, req, res, next) => {
-        res.status(500).send("Iternal Server Error");
-    });
-
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${ PORT }`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 setupServer();
 
