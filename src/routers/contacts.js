fdb0768274b3fad5e175b14676083fc4520af95b
vladislav-chihun <vladikkosmo@gmail.com>
2024-07-31 13:38:48 +0200

    fix

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { getContactById } from '../services/contacts.js';
+import { getContactByIdController } from '../controllers/contacts.js';
 import { getContacts } from '../controllers/contacts.js';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.status(200).send({ status: 200, message: 'Success' });
+});
 router.get('/contacts', ctrlWrapper(getContacts));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactById));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
 export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -20,45 +20,4 @@ async function getContactById(contactId) {
   }
 }
 
-async function createContact(contactData) {
-  try {
-    const newContact = new Contact(contactData);
-    await newContact.save();
-    return newContact;
-  } catch (error) {
-    console.error('Error while creating a new contact:', error);
-    throw error;
-  }
-}
-
-async function updateContact(contactId, contactData) {
-  try {
-    const updatedContact = await Contact.findByIdAndUpdate(
-      contactId,
-      contactData,
-      { new: true },
-    );
-    return updatedContact;
-  } catch (error) {
-    console.error(`Error while updating contact with id ${contactId}:`, error);
-    throw error;
-  }
-}
-
-async function deleteContact(contactId) {
-  try {
-    const deletedContact = await Contact.findByIdAndDelete(contactId);
-    return deletedContact;
-  } catch (error) {
-    console.error(`Error while deleting contact with id ${contactId}:`, error);
-    throw error;
-  }
-}
-
-export {
-  getAllContacts,
-  getContactById,
-  createContact,
-  updateContact,
-  deleteContact,
-};
+export { getAllContacts, getContactById };
