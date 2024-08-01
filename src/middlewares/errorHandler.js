27748068d393b95a1e9c3a204e682c8a374e43bf
vladislav-chihun <vladikkosmo@gmail.com>
2024-08-01 20:04:19 +0200

    final fix

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,8 @@
 function errorHandler(err, req, res, next) {
-  res.status(err.status || 500).json({
-    status: 'error',
-    message: err.message || 'Something went wrong',
-    data: err.data || null,
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.data,
   });
 }
 
--- src/server.js
+++ src/server.js
@@ -14,15 +14,15 @@ function setupServer() {
 
   app.use(router);
 
-  app.use(notFoundHandler);
-
-  app.use(errorHandler);
-
   app.use(express.json());
 
   app.use(cors());
 
   app.use(pino());
+
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
 
   app.get('/', (req, res) => {
     res.send('Success');
@@ -30,10 +30,6 @@ function setupServer() {
 
   app.use('*', (req, res, next) => {
     res.status(404).send({ message: 'Not found' });
-  });
-
-  app.use((error, req, res, next) => {
-    res.status(500).send('Iternal Server Error');
   });
 
   app.listen(PORT, () => {
